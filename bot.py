9c0c0e75d5c23c2a61597b771284939d585ca4ec
Shahadazub <andreigalkine@gmail.com>
2016-11-03 20:27:40 +0300

    версия для трансфера в юзера

--- bot.py
+++ bot.py
@@ -3,11 +3,25 @@ import config
 import telebot
 import xml_handler
 from time import sleep
+import random
 
 wow_message = "Воу, ну что ты сразу начинаешь. Полегче. Что хотел то?"
-message_to_send = 'Hi'
+
 
 bot = telebot.TeleBot(config.token)
+
+def get_new_riddle_id():
+    print('get_new_riddle_id started')
+    new_riddle_id = random.randrange(0,10000)
+    print('new riddle id = ' + str(new_riddle_id))
+    if xml_handler.riddle_id_is_free(str(new_riddle_id)):
+        print('riddle id is unique')
+        return str(new_riddle_id)
+    else:
+        print('riddle id already exist')
+        get_new_riddle_id()
+
+    
 
 
 
@@ -20,7 +34,7 @@ def ask_everyone_to_go(message): # function that send offer to go overwatch
 def add_gamer(message):
     print("add_me command recieved")
     if xml_handler.this_gamer_is_in_xml(message.chat.id):
-        bot.send_message(message.chat.id,"Эээ, братишь, ты уже в системе")
+        bot.send_message(message.chat.id,"Привет, братишь, а я тебя знаю")
     else:
         xml_handler.add_gamer_to_xml(message.chat.id)
         bot.send_message(message.chat.id,"You were added to the list")
@@ -39,14 +53,72 @@ def remove_gamer(message):
     else:
         bot.send_message(message.chat.id,"А ты ваще откуда нарисовался? Тебя тут небыло.")
 
+
+@bot.message_handler(commands=['add_riddle'])
+def add_riddle_to_xml(message):
+    if xml_handler.this_gamer_is_in_xml(message.chat.id):
+        bot.send_message(message.chat.id,"Привет, братишь, а я тебя знаю")
+    else:
+        xml_handler.add_gamer_to_xml(message.chat.id)
+        bot.send_message(message.chat.id,"You were added to the list")
+        sleep(3)
+        bot.send_message(message.chat.id,"bitch")
+    print(str(message.chat.id) + ' sent request to add riddle')
+    if message.chat.id != 278645740:
+        bot.send_message(message.chat.id,'В смысле, add_riddle?')
+    else:        
+        print('command add_riddle_to_xml recieved')
+        adding_riddle = True
+        param = 'name'
+        riddle_id = get_new_riddle_id()
+        xml_handler.create_new_riddle(riddle_id)
+        bot.send_message(message.chat.id,'Какое будет имя у загадки?')
+        xml_handler.set_gamer_mode(str(message.chat.id), 'name', str(riddle_id))
+        
+
+
 @bot.message_handler(content_types=["text"])
 def answer_to_user(message): #function that send answer
-    if message.text[:18] == 'В смысле, в смысле':
-        message_to_send = wow_message
+    param = xml_handler.gamer_mode(str(message.chat.id))
+    if xml_handler.this_gamer_is_in_xml(message.chat.id):
+        bot.send_message(message.chat.id,"Привет, братишь, а я тебя знаю")
     else:
-        message_to_send = "В смысле, " + message.text + "?"
-    bot.send_message(message.chat.id, message_to_send)
-    print(message.chat.id)
+        xml_handler.add_gamer_to_xml(message.chat.id)
+        bot.send_message(message.chat.id,"You were added to the list")
+        sleep(3)
+        bot.send_message(message.chat.id,"bitch")
+    if param == 'null':
+        if message.text[:18] == 'В смысле, в смысле':
+            message_to_send = wow_message
+            bot.send_message(message.chat.id, message_to_send)
+        else:
+            message_to_send = "В смысле, " + message.text + "?"
+            bot.send_message(message.chat.id, message_to_send)
+            print(message.chat.id)
+    else:
+        riddle_id = get_corrected_riddle_id(str(message.chat.id))
+        print('adding printed text to riddle')
+        if param == 'name':
+            xml_handler.insert_param_to_riddle(str(riddle_id), param, message.text)
+            xml_handler.set_gamer_mode(str(message.chat.id),'text', str(riddle_id))
+            bot.send_message(str(message.chat.id), 'Какой текст задачи?')
+        elif  param == 'text':
+            xml_handler.insert_param_to_riddle(str(riddle_id), param, message.text)
+            xml_handler.set_gamer_mode(str(message.chat.id),'answer')
+            bot.send_message(str(message.chat.id), 'Какой правильный ответ?')
+        elif param == 'answer':
+            xml_handler.insert_param_to_riddle(str(riddle_id), param, message.text)
+            xml_handler.set_gamer_mode(str(message.chat.id),'another answer')
+            bot.send_message(str(message.chat.id), 'Еще один ответ?')
+        elif param == 'another answer':
+            if message.text == 'Да':
+                xml_handler.set_gamer_mode(str(message.chat.id),'answer')
+                bot.send_message(message.chat.id, 'Какой правильный ответ?')
+            else:
+                xml_handler.set_gamer_mode(str(message.chat.id),'null')
+                bot.send_message(message.chat.id, 'Новая загадка сохранена')
+                #show_riddle(riddle_id)
+                
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
--- xml_handler.py
+++ xml_handler.py
@@ -1,5 +1,83 @@
 from lxml import etree
 
+list_of_riddle_elements = ['name', 'picture', 'text', 'answer', 'solved']
+
+
+def get_corrected_riddle_id(chat_id):
+    root = etree.parse('gamers.xml')
+    print('File parsed')
+    string_to_find = "gamer[@chat_id='" + chat_id + "']"
+    print('string_to_find created')
+    gamer = root.find(string_to_find)
+    return gamer.find("riddle_id").text
+    
+
+
+def create_new_riddle(riddle_id):
+    doc = etree.parse('riddles.xml')
+    print("riddles.xml parsed")
+    riddles = doc.getroot()
+    print("root element for riddles recieved")
+    riddle = etree.SubElement(riddles,'riddle')
+    print("Riddle subelement created")
+    riddle.set('id', riddle_id)
+    for subelement_name in list_of_riddle_elements: 
+        subelement = etree.SubElement(riddle, subelement_name)
+        subelement.text = '0'
+        print(subelement_name + ' created')
+    riddles.append(riddle)
+    print("riddle addded")
+    finaltext = riddles.getroottree()
+    print("created final tree")
+    finaltext.write('riddles.xml',encoding="utf-8",pretty_print=True)
+    print("file saved")
+    
+
+def gamer_mode(chat_id): 
+    print('gamer_mode started')
+    root = etree.parse('gamers.xml')
+    print('File parsed')
+    string_to_find = "gamer[@chat_id='" + chat_id + "']"
+    print('string_to_find created')
+    gamer = root.find(string_to_find)
+    print(gamer)
+    gamer_mode = gamer.find("mode")
+    print(gamer_mode)
+    return gamer_mode.text
+
+def set_gamer_mode(chat_id, mode_text, riddle_id):
+    root = etree.parse('gamers.xml')
+    print('File parsed')
+    string_to_find = "gamer[@chat_id='" + chat_id + "']"
+    print('string_to_find created')
+    gamer = root.find(string_to_find)
+    gamer_mode = gamer.find("mode")
+    gamer_mode.text = mode_text
+    riddle_number = gamer.find("riddle_id")
+    riddle_number.text = riddle_id
+    
+ 
+def insert_param_to_riddle(riddle_id, param, text):
+    root = etree.parse('riddles.xml')
+    print('riddles.xml parsed')
+    string_to_find = "riddle[@id='" + str(riddle_id) + "']"
+    riddle = root.find(string_to_find)
+    riddle.find(param).text = text 
+
+
+def riddle_id_is_free(riddle_id):
+    root = etree.parse('riddles.xml')
+    print('riddles.xml parsed')
+    string_to_find = "riddle[@id='" + str(riddle_id) + "']"
+    riddles = len(root.findall(string_to_find))
+    if riddles == 0:
+        print('Id is unique')
+        return True
+    else:
+        print('ID is in use')
+        return False
+
+
 def create_first_xml():
     gamers = etree.Element('gamers')
     gamer = etree.SubElement(gamers, 'gamer')
@@ -38,6 +116,9 @@ def add_gamer_to_xml(this_chat_id): #function adds elements about new user
     gamer.set('chat_id', str(this_chat_id))
     whant_to_play = etree.SubElement(gamer, 'whant_to_play')
     whant_to_play.text = 'NO'
+    gamer_mode = etree.SubElement(gamer, 'mode')
+    gamer_mode.text = 'null'
+    riddle_number = etree.SubElement(gamer, 'riddle_id')
     gamers.append(gamer)
     print("gamer addded")
     finaltext = gamers.getroottree()
@@ -54,3 +135,22 @@ def remove_gamer_from_xml(this_chat_id): #function deletes gamer element
     parent.remove(gamer)
     finaltext = parent.getroottree()
     finaltext.write('gamers.xml',pretty_print=True)
+
+def create_riddles_xml():
+    riddles = etree.Element('riddles')
+    riddle = etree.SubElement(riddles, 'riddle')
+    riddle.set('id', '1')
+    riddle_name = etree.SubElement(riddle, 'name')
+    riddle_name.text = 'Палки и Галки'
+    picture = etree.SubElement(riddle, 'picture')
+    picture.text = 'NO'
+    riddle_text = etree.SubElement(riddle, 'text')
+    riddle_text.text = 'Летели галки, сели на палки. Сядут по одной — галка лишняя, сядут по две — палка лишняя. Сколько было галок?'
+    answer = etree.SubElement(riddle, 'answer')
+    answer.text = '4'
+    answer = etree.SubElement(riddle, 'answer')
+    answer.text = 'четыре'
+    solved = etree.SubElement(riddle, 'solved')
+    solved.text = '0'
+    finaltext = riddles.getroottree()
+    finaltext.write('riddles.xml',encoding="utf-8",pretty_print=True)
